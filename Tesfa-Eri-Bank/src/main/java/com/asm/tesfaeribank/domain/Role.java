8dc7103b4a7961fa6fcc4bc6d7d636c9f446338a
NaodZere <naodmengis1@gmail.com>
2023-10-04 14:30:16 -0500

    I have updated Role

--- Tesfa-Eri-Bank/src/main/java/com/asm/tesfaeribank/domain/Role.java
+++ Tesfa-Eri-Bank/src/main/java/com/asm/tesfaeribank/domain/Role.java
@@ -1,20 +1,20 @@
-//package com.asm.tesfaeribank.domain;
-//
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.mongodb.core.mapping.Document;
-//
-//@Document(collection = "roles")
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Role {
-//
-//    @Id
-//    private String id;
-//
-//    private String name;
-//}
+package com.asm.tesfaeribank.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    private String id;
+
+    private String name;
+}
