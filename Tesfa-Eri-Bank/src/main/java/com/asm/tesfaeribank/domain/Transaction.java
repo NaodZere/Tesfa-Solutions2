fa0f810a370fc8165cf265a4b0d99a816fe19c8e
NaodZere <naodmengis1@gmail.com>
2023-10-04 14:51:44 -0500

    transactions service is compuleted

--- Tesfa-Eri-Bank/src/main/java/com/asm/tesfaeribank/domain/Transaction.java
+++ Tesfa-Eri-Bank/src/main/java/com/asm/tesfaeribank/domain/Transaction.java
@@ -1,38 +1,38 @@
-//package com.asm.tesfaeribank.domain;
-//
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.mongodb.core.mapping.Document;
-//
-//import java.time.LocalDateTime;
-//
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Document(collection = "transactions")
-//public class Transaction {
-//
-//    @Id
-//    private String id;
-//
-//    private LocalDateTime dateTime;
-//
-//    private String number;
-//
-//
-//    private Double amount;
-//
-//
-//    private String type;
-//    private String fromId;
-//
-//
-//    private String toId;
-//
-//    private Double balance;
-//
-//
-//}
+package com.asm.tesfaeribank.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "transactions")
+public class Transaction {
+
+    @Id
+    private String id;
+
+    private LocalDateTime dateTime;
+
+    private String number;
+
+
+    private Double amount;
+
+
+    private String type;
+    private String fromId;
+
+
+    private String toId;
+
+    private Double balance;
+
+
+}
