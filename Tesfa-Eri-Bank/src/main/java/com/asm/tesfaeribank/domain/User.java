d71d3e62e52fe579fcc8ace0ab87e29d07b4c720
Mussie <mussieayub@gmail.com>
2023-10-04 15:49:09 -0400

    committed

--- Tesfa-Eri-Bank/src/main/java/com/asm/tesfaeribank/domain/User.java
+++ Tesfa-Eri-Bank/src/main/java/com/asm/tesfaeribank/domain/User.java
@@ -1,30 +1,30 @@
-//package com.asm.tesfaeribank.domain;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.mongodb.core.index.Indexed;
-//import org.springframework.data.mongodb.core.mapping.Document;
-//import org.springframework.data.mongodb.core.mapping.DocumentReference;
-//
-//import java.util.List;
-//
-//@Document(collection = "users")
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class User {
-//
-//    @Id
-//    private String id;
-//
-//    @Indexed(unique = true)
-//    private String username;
-//    @Indexed(unique = true)
-//    private String email;
-//    private String password;
-//    @DocumentReference(lookup = "Role")
-//    private List<Role> roles;
-//
-//}
+package com.asm.tesfaeribank.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String username;
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+    @DocumentReference(lookup = "Role")
+    private List<Role> roles;
+
+}
